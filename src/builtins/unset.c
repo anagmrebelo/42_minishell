799e50abbddd7e5068d249aecaa5e4acc44a2dcb
Micaela Rollo <74556711+micarollo@users.noreply.github.com>
2023-01-24 12:43:58 +0000

    1 commit back. export TES\T=123 ERROR, unset -TEST OK

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -7,6 +7,20 @@ void    print_unset_error(char *str)
     ft_putendl_fd("': not a valid identifier", 2);
 }
 
+int first_u_check(char *str)
+{
+    if (str[0] == '-')
+    {
+        ft_putstr_fd("minishell: unset: ", 2);
+        ft_putchar_fd(str[0], 2);
+        ft_putchar_fd(str[1], 2);
+        ft_putendl_fd(": invalid option", 2);
+        ft_putendl_fd("unset: usage: unset [-f] [-v] [-n] [name ...]", 2);
+        return (1);
+    }
+    return (0);
+}
+
 void    unset_var(t_env *env, char *str)
 {
     t_env   *next;
@@ -46,6 +60,11 @@ int    ft_unset(t_env *env, char **args)
     i = 1;
     while (args[i])
     {
+        if (first_u_check(args[i]))
+        {
+            return (1);
+            break ;
+        }
         if(check_var(args[i]))
             unset_var(env, args[i]);
         else
